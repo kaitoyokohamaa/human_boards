e731ce1d1f05f7473d9873bcfe275831fbe910c6
yoko <s1A121700885@toyo.jp>
2021-07-27 18:47:29 +0900

    fix:typo

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -21,7 +21,7 @@ export default function detail() {
         <div className="col s12 m7">
           <div className="card">
             <div className="card-content">
-              <h8>ニックネーム</h8>
+              <h4>ニックネーム</h4>
               <p>勉強を親から急かされるのは人権の侵害ですか？</p>
             </div>
             <div className="m-auto flex"></div>
